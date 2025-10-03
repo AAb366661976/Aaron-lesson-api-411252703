dcbe0f4c70f05fe0cd5b534a99c0d03814664e22
bella <134810151+AAb366661976@users.noreply.github.com>
2025-10-03 11:28:11 +0800

    add home.html

--- app.py
+++ app.py
@@ -15,5 +15,33 @@ def show_user_profile(username):
     print('type(username)=> ',type(username))
     return 'String => {}'.format (username)
 
+@app.route("/user/<int:id>",methods=['GET'])
+def show_user_id(id):
+    print('type(id)=> ',type(id))
+    return 'Int => {}'.format (id)
+
+@app.route("/user/<float:version>",methods=['GET'])
+def show_user_version(version):
+    print('type(version)=> ',type(version))
+    return 'Float => {}'.format (version)
+
+@app.route("/appinfo")
+def show_appinfo():
+    return "<html><body><h1>APPINFO</h1></body></html>"
+
+@app.route("/htmlinfo")
+def show_htmlinfo():
+    return """
+    <html>
+        <head>
+            <title>HTML INFO</title>
+        </head>
+        <body>
+            <h1>HTML INFO</h1>
+            <p>This is a paragraph.</p>
+        </body>
+    </html>
+    """
+
 if __name__ == "__main__":
     app.run('0.0.0.0',debug=True)
