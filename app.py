b6a9920faf4e116189c728fb3e50b5c22a1bb557
bella <134810151+AAb366661976@users.noreply.github.com>
2025-10-03 10:46:16 +0800

    網頁更新username route

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def home():
 def hola():
     return "Hola, Mundo!"
 
+@app.route("/user/<string:username>",methods=['GET'])
+def show_user_profile(username):
+    print('type(username)=> ',type(username))
+    return 'String => {}'.format (username)
+
 if __name__ == "__main__":
     app.run('0.0.0.0',debug=True)
